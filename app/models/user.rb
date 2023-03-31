0b82fcf4cc47db1a3e30d6aa7becaad29f36df8f
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-03-31 14:09:36 -0400

    seeded

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,43 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, uniqueness: true, length: { in: 3..30 }, format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+  validates :email, uniqueness: true, length: { in: 3..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(credential, password)
+    if credential.match(URI::MailTo::EMAIL_REGEXP)
+      user = User.find_by(email: credential)
+    else
+      user = User.find_by(username: credential)
+    end
+
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil 
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    token = SecureRandom::urlsafe_base64
+      while User.exists?(session_token: token)
+        token = SecureRandom::urlsafe_base64
+      end
+      token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
 end
