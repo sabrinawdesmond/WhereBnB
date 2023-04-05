fa63d268069af2208be258fd8b8928e1e3382691
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-04-05 11:54:11 -0400

    finished logo styling

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
-import LoginFormModal from '../LoginFormModal';
-import logoImage from './logo.png'
-import SignupFormModal from '../SignupFormModal';
-import menuIcon from './menuIcon.png'
-import profileIcon from'./profileIcon.png'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+import LoginFormModal from "../LoginFormModal";
+import logoImage from "./logo.png";
+import SignupFormModal from "../SignupFormModal";
+import menuIcon from "./menuIcon.png";
+import profileIcon from "./profileIcon.png";
+import { Link } from "react-router-dom";
 
 function Navigation() {
-  const sessionUser = useSelector(state => state.session.user);
+  const sessionUser = useSelector((state) => state.session.user);
 
   let sessionLinks;
   if (sessionUser) {
-    sessionLinks = (
-      <ProfileButton user={sessionUser} />
-    );
+    sessionLinks = <ProfileButton user={sessionUser} />;
   } else {
     sessionLinks = (
       <>
@@ -29,30 +27,28 @@ function Navigation() {
 
   return (
     <>
-    <div className="navBar">
-
-    <Link to="/">
-        <div className="logo-container">
-        <img src={logoImage} alt="logo"  style={{ width: '40px', height: '40px' }} />
-          <div className="title-container">
-            <h2>Wherebnb</h2>
+      <div className="navBar">
+        <Link to="/">
+          <div className="logo-container">
+            <img src={logoImage} alt="logo" />
+            <div className="title-container">
+              <h2>Wherebnb</h2>
+            </div>
           </div>
-        </div>
-      </Link>
-    
+        </Link>
         <h3>Search</h3>
-      <div className="navLinks">
-        <div class='profileDrop'>
-      <a href="/">
-        <img src={menuIcon} alt="logo" />
-        </a>
-        <a href="/">
-        <img src={profileIcon} alt="logo"  />
-        </a>
+        <div className="navLinks">
+          <div class="profileDrop">
+            <a href="/">
+              <img src={menuIcon} alt="logo" />
+            </a>
+            <a href="/">
+              <img src={profileIcon} alt="logo" />
+            </a>
+          </div>
+          {sessionLinks}
         </div>
-        {sessionLinks}
       </div>
-    </div>
     </>
   );
 }
