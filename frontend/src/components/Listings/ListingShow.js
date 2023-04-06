7d38c6f1b9081154a89ce13c1e972d7f2d44bbdc
SWD <116519976+sabrinawdesmond@users.noreply.github.com>
2023-04-06 15:47:29 -0400

    ListingIndex and ListingIndexItem

---
+++ frontend/src/components/Listings/ListingShow.js
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchListing, getListing } from "../../store/listings";
+import { useEffect } from "react";
+
+const ListingShow = () => {
+  const dispatch = useDispatch();
+  const listingId = useParams();
+  const listing = useSelector(getListing(listingId));
+
+  useEffect(() => {
+    dispatch(fetchListing(listingId))
+  }, [listingId, dispatch])
+
+  return(
+    <>
+    </>
+  )
+
+};
+
+export default ListingShow;
